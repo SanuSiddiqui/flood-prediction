c46e4af43c1fc40e06b57860304eba7d1d0ecb33
Shanu Siddiqui <139368222+karim-071@users.noreply.github.com>
2024-12-09 22:29:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ import plotly.express as px
 # Set Streamlit page configuration - should be the first Streamlit command
 st.set_page_config(page_title="Flood Prediction", page_icon="🌊", layout="wide")
 
-hide_github_icon = “”"
-
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-“”"
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+# Add custom CSS to hide the header (including the GitHub icon)
+hide_header_css = """
+<style>
+    header {display: none;}
+</style>
+"""
+st.markdown(hide_header_css, unsafe_allow_html=True)
 
 # Load Data
 @st.cache_data
